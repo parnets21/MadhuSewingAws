67795406b84f1332fdb77406cfcb99c301e285ee
parnets21 <parnetstech21@gmail.com>
2025-11-03 11:12:04 +0530

    qwerty

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const cors = require('cors');  
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+  
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Database Connection
 const connectDB = async () => {
   try {
